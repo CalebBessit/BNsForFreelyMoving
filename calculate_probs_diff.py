0f9a3242eb2345395fe2fd2a96221e3d953b3c48
Matthew Dean <pvzmatthew@gmail.com>
2025-10-09 22:50:23 +0200

    Use most optimal stat points for probabilities.

--- calculate_probs_diff.py
+++ calculate_probs_diff.py
@@ -6,7 +6,7 @@
 import os
 import numpy as np
 
-subjects = 10
+subjects = 8
 
 overall_fr, overall_nfr = None, None
 overall_labels = None
@@ -50,23 +50,51 @@ chosen_features = alpha_features + behavioral_features + erp_features
 
 
 all_probs = { feature:[] for feature in chosen_features}
+all_stats = { feature:[] for feature in chosen_features}
 chosen_indices = [original_features.index(feature) for feature in chosen_features]
 
 overall_fr, overall_nfr = overall_fr[:,chosen_indices], overall_nfr[:,chosen_indices]
 combined = np.vstack((overall_fr, overall_nfr))
 
 fr_median, nfr_median = np.median(overall_fr, axis=0), np.median(overall_nfr, axis=0)
-fr_diffs, nfr_diffs = np.abs(combined-fr_median), np.abs(combined-nfr_median)
+fr_mean, nfr_mean = np.mean(overall_fr, axis=0), np.mean(overall_nfr, axis=0)
+fr_std, nfr_std = np.std(overall_fr, axis=0), np.std(overall_nfr, axis=0)
 
-bin_digits = (fr_diffs<nfr_diffs).astype(int)
+# fr_diffs, nfr_diffs = np.abs(combined-fr_median), np.abs(combined-nfr_median)
+
+mask = [int(b) for b in f"{243:08b}"]
+
+fr_stats = [np.min(overall_fr, axis=0), np.max(overall_fr, axis=0), fr_median, np.percentile(overall_fr, 25, axis=0), np.percentile(overall_fr, 75, axis=0), fr_mean, fr_mean-fr_std, fr_mean+fr_std]
+nfr_stats = [np.min(overall_nfr, axis=0), np.max(overall_nfr, axis=0), nfr_median, np.percentile(overall_nfr, 25, axis=0), np.percentile(overall_nfr, 75, axis=0), nfr_mean, nfr_mean-nfr_std, nfr_mean+nfr_std]
+
+fr_stats = [v for v, m in zip(fr_stats, mask) if m]
+nfr_stats = [v for v, m in zip(nfr_stats, mask) if m]
+
+fr_min_diffs, nfr_min_diffs = None, None
+for i in range(len(fr_stats)):
+    fr_diffs, nfr_diffs = fr_stats[i]-combined, nfr_stats[i]-combined
+    if i==0:
+        fr_diffs, nfr_diffs = -fr_diffs, -nfr_diffs   
+    elif i>1: 
+        fr_diffs, nfr_diffs = np.abs(fr_diffs), np.abs(nfr_diffs)
+            
+    fr_diffs[fr_diffs < 0], nfr_diffs[nfr_diffs < 0] = np.inf, np.inf
+
+    if fr_min_diffs is None:
+        fr_min_diffs, nfr_min_diffs = fr_diffs, nfr_diffs
+    else:
+        fr_min_diffs, nfr_min_diffs = np.minimum(fr_min_diffs, fr_diffs), np.minimum(nfr_min_diffs, nfr_diffs)
+            
+    fr_bin_digits = (fr_min_diffs<=nfr_min_diffs).astype(int)
+    nfr_bin_digits = (nfr_min_diffs<=fr_min_diffs).astype(int)
 
 overall_labels = np.reshape(overall_labels,(overall_labels.shape[0],1)) # Reshape labels so that they can be concatenated with features
-combined = np.hstack((overall_labels, bin_digits)) #Recombine labels and features
 
+combined = np.hstack((overall_labels, fr_bin_digits)) #Recombine labels and features
 fr  = combined[combined[:,0] == 3.0][:,1:]
+
+combined = np.hstack((overall_labels, nfr_bin_digits)) #Recombine labels and features
 nfr = combined[combined[:,0] == 4.0][:,1:]
-
-
 
 
 # Can now calculate probabilities based on counts
@@ -84,7 +112,17 @@ for i in range(len(chosen_indices)):
     feature = chosen_features[i]
     all_probs[feature] = [ [feat_off_nfr[i], feat_on_nfr[i]], [feat_off_fr[i],feat_on_fr[i]] ]
 
+print(all_probs)
+print([val[1] for val in all_probs.values()])
+values = []
+try:
+    for val in all_probs.values():
+        values.append(val[1][1])
+except:
+    pass
 
+print(np.average(values))
 
-np.save( os.path.join(param_dir, "probabilities.npy"), np.array([all_probs]) )
+np.save( os.path.join(param_dir, "probabilities.npy"), np.array([all_probs]))
+np.save( os.path.join(param_dir, "statistics.npy"), np.array([fr_stats, nfr_stats]))
 print("Done.")
--- raw_to_bin_digits.py
+++ raw_to_bin_digits.py
@@ -4,8 +4,30 @@
 
 import os
 import numpy as np
+# import argparse
+
+# parser = argparse.ArgumentParser(description="Example command-line program")
+# parser.add_argument("stat_index", type=int, help="Path to the input file")        # positional
+# args = parser.parse_args()
+
 
 subjects = (10,15)
+
+original_features = ['p3_mean', 'p3_std', 'alpha_mean', 'alpha_var',
+            'rt_mean', 'rt_var', 'ra', 'p3_log',
+            'p3_shan', 'p3_sure', 'p3_skew', 'p3_kurt', 
+            'alpha_log', 'alpha_shan', 'alpha_sure', 'alpha_skew', 
+            'alpha_kurt']
+
+alpha_features      = ['alpha_var','alpha_kurt','alpha_shan']
+behavioral_features = ['rt_mean','rt_var','ra']
+erp_features        = ['p3_kurt']
+
+chosen_features = alpha_features + behavioral_features + erp_features
+chosen_indices = [original_features.index(feature) for feature in chosen_features]
+
+all_probs = { feature:[] for feature in chosen_features}
+prob_ave = []
 
 outdir = "test_bin_digits"
 os.makedirs(outdir, exist_ok=True)
@@ -16,16 +38,83 @@ for subject in range(subjects[0], subjects[1]):
     fr  = data[data[:,3] == 3.0][:,4:]
     nfr = data[data[:,3] == 4.0][:,4:]
 
+    mask = data[data[:,3] == 3.0][:,3]
+    
     features = data[:,4:]
 
-    fr_medians, nfr_medians = np.median(fr, axis=0), np.median(nfr, axis=0)
-    fr_diffs, nfr_diffs = np.abs(features-fr_medians), np.abs(features-nfr_medians)
+    # fr_medians, nfr_medians = np.median(fr, axis=0), np.median(nfr, axis=0)
+    # fr_diffs, nfr_diffs = np.abs(features-fr_medians), np.abs(features-nfr_medians)
     
-    bin_digits = (fr_diffs < nfr_diffs).astype(int)
+    # bin_digits = (fr_diffs < nfr_diffs).astype(int)
+    fr_median, nfr_median = np.median(fr, axis=0), np.median(nfr, axis=0)
+    fr_mean, nfr_mean = np.mean(fr, axis=0), np.mean(nfr, axis=0)
+    fr_std, nfr_std = np.std(fr, axis=0), np.std(nfr, axis=0)
+
+    # fr_diffs, nfr_diffs = np.abs(combined-fr_median), np.abs(combined-nfr_median)
+    stats = np.load(f"parameter_data/statistics.npy")
+    print(stats)
+    num_stats = len(stats[0])
+    fr_stats, nfr_stats = [], []
+    for i in range(num_stats):
+        fr_stats.append([])
+        nfr_stats.append([])
+    
+    for i in range(len(features[0])): 
+        for j in range(num_stats):
+            if i in chosen_indices:
+                index = chosen_indices.index(i)
+                fr_stats[j].append(stats[0][j][index])
+                nfr_stats[j].append(stats[1][j][index])  
+            else:
+                fr_stats[j].append(np.inf)
+                nfr_stats[j].append(np.inf)
+    
+    fr_min_diffs, nfr_min_diffs = None, None
+    for i in range(len(fr_stats)):
+        fr_diffs, nfr_diffs = np.array([fr_stats[i]])-features, np.array([nfr_stats[i]])-features
+        if i==0:
+            fr_diffs, nfr_diffs = -fr_diffs, -nfr_diffs   
+        elif i>1: 
+            fr_diffs, nfr_diffs = np.abs(fr_diffs), np.abs(nfr_diffs)
+                
+        fr_diffs[fr_diffs < 0], nfr_diffs[nfr_diffs < 0] = np.inf, np.inf
+
+        if fr_min_diffs is None:
+            fr_min_diffs, nfr_min_diffs = fr_diffs, nfr_diffs
+        else:
+            fr_min_diffs, nfr_min_diffs = np.minimum(fr_min_diffs, fr_diffs), np.minimum(nfr_min_diffs, nfr_diffs)
+                
+    bin_digits = (fr_min_diffs<=nfr_min_diffs).astype(int)
+        
     combined_data = np.hstack( (data[:,:4], bin_digits))
 
     np.save( os.path.join(outdir, f"S1{subject_id}_binary_digit_data.npy"), combined_data )
     print(f"Done with {subject_id}.")
+    
+#     labels = data[:,3]
+#     labels = np.reshape(labels,(labels.shape[0],1))
+#     combined = np.hstack( (labels, bin_digits))
+#     fr  = combined[combined[:,0] == 3.0][:,1:]
+#     nfr = combined[combined[:,0] == 4.0][:,1:]
+    
+#     feat_on_fr, feat_on_nfr   = np.sum(fr, axis=0), np.sum(nfr, axis=0)
+#     feat_off_fr, feat_off_nfr = fr.shape[0]-feat_on_fr, nfr.shape[0]-feat_on_nfr
+
+#     feat_on_fr, feat_on_nfr = feat_on_fr/fr.shape[0], feat_on_nfr/nfr.shape[0]
+#     feat_off_fr, feat_off_nfr = feat_off_fr/fr.shape[0], feat_off_nfr/nfr.shape[0]
+
+#     # Probabilities counted, now save to file
+
+#     for i in range(len(chosen_indices)):
+#         feature = chosen_features[i]
+#         all_probs[feature] = [ [feat_off_nfr[i], feat_on_nfr[i]], [feat_off_fr[i],feat_on_fr[i]] ]
+
+#     prob_ave.append(np.average([val[1][1] for val in all_probs.values()]))
+
+# valid_file = open('validation.txt','a')
+# valid_file.write(f"{args.stat_index} : {np.average(prob_ave)}")
+# valid_file.write("\n")
+    
     # print(combined_data.shape)
 
 
